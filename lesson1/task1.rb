a2c327627e068dba052bc79f73a9d1d431152ad6
Siyanko Maksim <sianko0608@gmail.com>
2024-07-02 13:53:54 +0300

    Add task1

--- lesson1/task1.rb
+++ lesson1/task1.rb
@@ -1,3 +1,9 @@
-a =1
-b=2
-puts a+b
+print("Введите имя, фамилию и возраст")
+name = gets.to_s
+surname = gets.to_s
+age = gets.to_i
+ if age>18
+    print("Привет, "+name+" "+surname+". Тебе меньше 18, можно начинать учиться программировать")
+ else
+    print("Привет, "+name+" "+surname+", Самое время работать.")
+ end
