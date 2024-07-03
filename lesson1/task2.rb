c0a9d15d30cc211201cd3ea4cce9ee5cd77f245c
Siyanko Maksim <sianko0608@gmail.com>
2024-07-03 10:46:32 +0300

    Added task2

---
+++ lesson1/task2.rb
@@ -0,0 +1,12 @@
+def foobar(num1, num2)
+    if num1==20 or num2==20
+        return num2
+    else
+        return num1+num2
+    end
+end
+
+puts "Введите два числа "
+num1 = gets.to_i
+num2 = gets.to_i
+puts foobar(num1, num2)
