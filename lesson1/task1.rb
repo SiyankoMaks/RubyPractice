8d556631f5bcbbad5e7507f845512c29d1187dc5
Siyanko Maksim <sianko0608@gmail.com>
2024-07-03 10:39:01 +0300

    Corrected task 1

--- lesson1/task1.rb
+++ lesson1/task1.rb
@@ -1,9 +1,13 @@
-print("Введите имя, фамилию и возраст")
-name = gets.to_s
-surname = gets.to_s
+def greeting(name, surname, age)
+   if age<18
+      puts "Привет, #{name} #{surname}. Тебе меньше 18, можно начинать учиться программировать"
+   else
+      puts "Привет, #{name} #{surname}, Самое время работать."
+   end
+end
+
+puts "Введите имя, фамилию и возраст"
+name = gets.chomp
+surname = gets.chomp
 age = gets.to_i
- if age>18
-    print("Привет, "+name+" "+surname+". Тебе меньше 18, можно начинать учиться программировать")
- else
-    print("Привет, "+name+" "+surname+", Самое время работать.")
- end
+greeting(name, surname, age)
